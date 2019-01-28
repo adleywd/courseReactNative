076c632c3b744cd255bb6e35e527df39e4196f3a
adleywd <adleydamaceno@gmail.com>
2019-01-28 17:07:12 -0200

    First Commit

---
+++ src/Menu.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { createDrawerNavigator } from'react-navigation'
+
+import Simples from './componentes/Simples'
+import ParImpar from './componentes/ParImpar'
+import { Inverter, MegaSena } from './componentes/Multi'
+import Contador from './componentes/contador'
+
+export default createDrawerNavigator({
+    Contador: {
+        screen: () => <Contador numeroInicial={100}/>
+    },
+    MegaSena: {
+        screen: () => <MegaSena numeros={8} />,
+        navigationOptions: { title:'Mega Sena' }
+    },
+    Inverter: {
+        screen: () => <Inverter texto='React Nativo!' />
+    },
+    ParImpar: {
+        screen: () => <ParImpar numero={30} />,
+        navigationOptions: { title: 'Par & Ímpar' }
+    },
+    Simples: {
+        screen: () => <Simples texto='Flexível!!' />
+    }
+}, {drawerWidth: 300})
